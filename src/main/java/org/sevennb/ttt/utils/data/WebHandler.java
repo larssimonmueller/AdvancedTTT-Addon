b489738af4254f5ff1a4b7e976184dd71dbe7772
Gedankenleid <Gedankenleid@users.noreply.github.com>
2019-01-03 23:29:20 +0100

    Initial commit

--- src/main/java/org/sevennb/ttt/utils/data/WebHandler.java
+++ src/main/java/org/sevennb/ttt/utils/data/WebHandler.java
@@ -2,35 +2,45 @@ package org.sevennb.ttt.utils.data;
 
 import net.minecraft.client.Minecraft;
 import org.sevennb.ttt.TTTAddon;
+import org.sevennb.ttt.utils.TextColor;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 public class WebHandler {
 
     public static void send(String role){
         if(TTTAddon.DATA){
             String username = Minecraft.getMinecraft().thePlayer.getName();
-            try {
-                read(new URL("https://7nb.org/mod.php?name="+username+"&role="+role));
-            } catch (MalformedURLException e) {}
+            System.out.println(TextColor.ANSI_RED+username+TextColor.ANSI_RESET);
+            read("https://7nb.org/mod/index.php?name="+username+"&role="+role);
         }
     }
 
-    public static String read(URL url){
-        Scanner scan = null;
-        try {
-            scan = new Scanner(url.openStream());
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static String read(String url){
+        StringBuilder response=new StringBuilder();
+        String USER_AGENT="Mozilla/5.0",inputLine;
+        BufferedReader in=null;
+
+        try
+        {
+            HttpURLConnection con=(HttpURLConnection)new URL(url).openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Accept-Charset","UTF-8");
+            con.setRequestProperty("User-Agent",USER_AGENT);
+            int responseCode=con.getResponseCode();
+            in=new BufferedReader(new InputStreamReader(con.getInputStream()));
+            while ((inputLine=in.readLine())!=null) { response.append(inputLine); }
+            in.close();
         }
-        if (scan != null) {
-            while(scan.hasNextLine()) {
-                return scan.nextLine();
-            }
+        catch (Exception e) { e.printStackTrace(); }
+        finally
+        {
+            try { if (in!=null) in.close(); }
+            catch (Exception ex) { ex.printStackTrace(); }
         }
-        return "invalid";
+        return response.toString();
     }
 }
