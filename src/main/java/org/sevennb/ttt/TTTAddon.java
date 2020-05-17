522098acfbb86e1cc79689390a71165566bdf730
Gedankenleid <Gedankenleid@users.noreply.github.com>
2020-05-17 09:19:38 +0200

    + Webpanel Icon Requests

--- src/main/java/org/sevennb/ttt/TTTAddon.java
+++ src/main/java/org/sevennb/ttt/TTTAddon.java
@@ -51,6 +51,9 @@ public class TTTAddon extends LabyModAddon {
             server.createContext("/localstyle", new LocalStyleHandler());
             server.createContext("/bootstrap", new BootstrapHandler());
             server.createContext("/jquery", new JQueryHandler());
+            server.createContext("/roleicon", new RolleIconHandler());
+            server.createContext("/testsicon", new TestsIconHandler());
+            server.createContext("/trapicon", new TrapIconHandler());
             server.createContext("/falle", new FalleHandler());
             server.createContext("/rolle", new RolleHandler());
             server.createContext("/one", new TestOneHandler());
---
+++ src/main/java/org/sevennb/ttt/panel/TestsIconHandler.java
@@ -0,0 +1,27 @@
+package org.sevennb.ttt.panel;
+
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.*;
+
+public class TestsIconHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange he) throws IOException {
+        Headers headers = he.getResponseHeaders();
+        headers.add("Content-Type", "image/svg+xml");
+
+        File file = new File ("LabyMod/addons-1.8/AdvancedTTT/assets/tests.svg");
+        byte[] bytes  = new byte [(int)file.length()];
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        bufferedInputStream.read(bytes, 0, bytes.length);
+
+        he.sendResponseHeaders(200, file.length());
+        OutputStream outputStream = he.getResponseBody();
+        outputStream.write(bytes, 0, bytes.length);
+        outputStream.close();
+    }
+}
